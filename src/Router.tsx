54b298137664a0d152612bf944ae6ad044145e85
Jhon Cruz <jhonskaz@gmail.com>
2024-03-11 17:48:19 -0300

    Adiciona listagem de checkboxes

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,7 +5,7 @@ import { SignUpForm } from "./pages/singup-form";
 import { NotFound } from "./pages/not-found";
 import { PrivateRoutes } from "./ProtectedRoutes/protected-routes";
 import { DateTimePickerDemoWithFormShadcnUI } from "./components/time-picker-demo-in-form-shadcn";
-import { DinamicForm } from "./testes/dinamic-form";
+import { CheckboxReactHookFormMultiple } from "./testes/checkboxes";
 export function Router() {
   return (
     <Routes>
@@ -19,7 +19,7 @@ export function Router() {
         path="/datetime"
         element={<DateTimePickerDemoWithFormShadcnUI />}
       />
-      <Route path="/test" element={<DinamicForm />} />
+      <Route path="/test" element={<CheckboxReactHookFormMultiple />} />
     </Routes>
   );
 }
